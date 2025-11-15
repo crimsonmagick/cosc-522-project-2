c43c0b8834fd457b2f8a27425b4dc768350a6167
Welby Seely <welbyseely@gmail.com>
2025-11-15 13:52:09 -0500

    Created parallel domain structure for streams

--- include/domain_stream.h
+++ include/domain_stream.h
@@ -1,50 +1,37 @@
-// /**
-// *  WIP service interface for managing interactions between clients and servers while maintaining an open socket and
-// *  abstracting serialization and deserialization.
-//  */
-//
-// #ifndef COSC522_LODI_DOMAIN_STREAM_H
-// #define COSC522_LODI_DOMAIN_STREAM_H
-//
-// #include <netinet/in.h>
-// #include "domain.h"
-//
-// typedef struct StreamDomainService StreamDomainService;
-//
-// typedef struct DomainServiceHandle {
-//   DomainService *domainService;
-// } DomainServiceHandle;
-//
-// typedef struct MessageSerializer {
-//   size_t messageSize;
-//
-//   int (*serializer)(void *, char *);
-// } MessageSerializer;
-//
-// typedef struct MessageDeserializer {
-//   size_t messageSize;
-//
-//   int (*deserializer)(char *, void *);
-// } MessageDeserializer;
-//
-//
-// typedef struct DomainServiceOpts {
-//   char *localPort;
-//   int timeoutMs;
-//   MessageSerializer outgoingSerializer;
-//   MessageDeserializer incomingDeserializer;
-// } DomainServiceOpts;
-//
-// int startService(const DomainServiceOpts options, DomainServiceHandle **handle);
-//
-// int stopService(DomainServiceHandle **handle);
-//
-// int toDomainHost(DomainServiceHandle *handle, void *message, struct sockaddr_in *hostAddr);
-//
-// int fromDomainHost(DomainServiceHandle *handle, void *message, struct sockaddr_in *hostAddr);
-//
-// int changeTimeout(DomainServiceHandle *handle, int timeoutMs);
-//
-//
-//
-// #endif
+/**
+*  WIP service interface for managing interactions between clients and servers while maintaining an open socket and
+*  abstracting serialization and deserialization.
+ */
+
+#ifndef COSC522_LODI_DOMAIN_STREAM_H
+#define COSC522_LODI_DOMAIN_STREAM_H
+
+#include <netinet/in.h>
+#include "domain.h"
+
+typedef struct StreamDomainService StreamDomainService;
+
+typedef struct StreamDomainServiceHandle {
+  StreamDomainService *streamDomainService;
+} StreamDomainServiceHandle;
+
+typedef struct StreamDomainServiceOpts {
+  char *localPort;
+  int timeoutMs;
+  char *remotePort;
+  char *remoteHost;
+  MessageSerializer outgoingSerializer;
+  MessageDeserializer incomingDeserializer;
+} StreamDomainServiceOpts;
+
+int startStreamService(const StreamDomainServiceOpts options, StreamDomainServiceOpts **handle);
+
+int stopService(StreamDomainServiceHandle **handle);
+
+int toStreamDomainHost(StreamDomainServiceHandle *handle, void *message, struct sockaddr_in *hostAddr);
+
+int fromStreamDomainHost(StreamDomainServiceHandle *handle, void *message, struct sockaddr_in *hostAddr);
+
+int changeStreamTimeout(StreamDomainServiceHandle *handle, int timeoutMs);
+
+#endif
